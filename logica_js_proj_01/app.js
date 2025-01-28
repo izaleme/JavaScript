4d98c3d74156b6c13dc15b05417d7360e425dd4d
izaleme <izaleme00@gmail.com>
2025-01-28 10:46:42 -0300

    Base do js do jogo

--- logica_js_proj_01/app.js
+++ logica_js_proj_01/app.js
@@ -1,1 +1,10 @@
-alert('Hello World');
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí! Você acertou!');
+}
+else {
+    console.log('Poxa, você errou!');
+}
