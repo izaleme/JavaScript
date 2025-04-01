fb25ec36efc09e39aaf086a218ab26626f427c39
izaleme <izaleme00@gmail.com>
2025-04-01 13:50:08 -0300

    Melhorando o código do desafio 3

--- logica_js_desafio_03/app.js
+++ logica_js_desafio_03/app.js
@@ -1,25 +1,41 @@
 // DESAFIO: TORNAR O CODIGO DINÂMICO
-
 alert('Boas vindas ao jogo do número secreto!');
-let numMax = prompt('Digite o valor máximo possível para o jogo de adivinhação:');
-let numeroSecreto = parseInt(Math.random() * numMax + 1);
-let tentativas = 1;
-let chute;
+let continueGame = true;
 
-while(chute != numeroSecreto){''
-    chute = prompt(`Escolha um numero entre 1 e ${numMax}:`);
+while (continueGame) {
+    let numMax = parseInt(prompt('Digite o valor máximo possível para o jogo de adivinhação:'));
+    let numeroSecreto = Math.floor(Math.random() * numMax) + 1;
+    let tentativas = 1;
+    let chute;
 
-    if (chute == numeroSecreto){
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
+    if (numMax === null || isNaN(numMax) || numMax <= 0) // Valida se é um número válido
+    {
+        alert('Insira um número válido maior que zero para jogar!');
+        continue; // Retorna ao início do loop
+    }
+    
+    while(chute != numeroSecreto){''
+        chute = prompt(`Escolha um numero entre 1 e ${numMax}:`);
+    
+        if (chute == null){
+            continueGame = false;
+            alert('Jogo finalizado.');
+            break; // Encerra o loop do jogo
         }
+        
+        chute = parseInt(chute); // Converte chute para número
 
-        tentativas ++;
+        if (chute == numeroSecreto){
+            alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} após ${tentativas} ${tentativas === 1 ? 'tentativa' : 'tentativas'}!`);
+            break;
+        } else {
+            if (chute > numeroSecreto) {
+                alert(`O número secreto é menor que ${chute}`);
+            } else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
+    
+            tentativas ++;
+        }
     }
 }
-
-alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} após ${tentativas} ${tentativas === 1 ? 'tentativa' : 'tentativas'}!`);
